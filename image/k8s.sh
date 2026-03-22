7bfa844d4e7df0bd5a082d08cdee3b52cb9ca4d3
wyccsc <wyc890619@gmail.com>
2026-03-22 20:06:04 +0800

    frontend

--- image/k8s.sh
+++ image/k8s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker pull wyccsc/kge_api_service:1.0.0
-docker pull wyccsc/kge_frontend_service:1.0.0
-docker pull wyccsc/kge_model_service:1.0.0
+docker pull wyccsc/kge_api_service:1.0.3
+docker pull wyccsc/kge_frontend_service:1.0.3
+docker pull wyccsc/kge_model_service:1.0.3
